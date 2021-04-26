60c39293740a11629d3b7b3d11be87bee785ea3b
mfcbentes <mfcbentes.stm@gmail.com>
2021-04-26 20:03:34 -0300

    Implementar função Limpar

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
       <button class="botao" v-on:click="iniciar">{{ botao }}</button>
       <button class="botao" v-on:click="limpar">LIMPAR</button>
     </div>
+    <div class="list" v-show="historico > 0">
+      <ul>
+        <li>VOCÊ FEZ UMA PAUSA EM: 00:00:03</li>
+      </ul>
+      <button>Limpar histórico</button>
+    </div>
   </div>
 </template>
 
@@ -14,12 +20,13 @@ export default {
   name: "App",
   data() {
     return {
-      numero: 0,
+      numero: "00:00:00",
       botao: "INICIAR",
       timer: null,
       ss: 0,
       mm: 0,
       hh: 0,
+      historico: [],
     };
   },
   methods: {
@@ -35,7 +42,17 @@ export default {
         this.botao = "PAUSAR";
       }
     },
-    limpar() {},
+    limpar() {
+      if (this.timer !== null) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+      this.ss = 0;
+      this.mm = 0;
+      this.hh = 0;
+      this.numero = "00:00:00";
+      this.botao = "INICIAR";
+    },
     rodarTimer() {
       this.ss++;
       if (this.ss == 59) {
@@ -105,5 +122,29 @@ export default {
 .botao:hover {
   opacity: 0.8;
   transition: all 0.5s;
+}
+
+ul {
+  text-align: center;
+  padding: 0px;
+}
+
+ul li {
+  margin-top: 4px;
+  padding: 15px;
+  background-color: rgb(70, 70, 70);
+  list-style: none;
+  color: #fff;
+  font-size: 18px;
+  border-radius: 6px;
+}
+
+.list button {
+  cursor: pointer;
+  border: 0;
+  background-color: #fff;
+  padding: 8px;
+  border-radius: 5px;
+  margin-bottom: 12px;
 }
 </style>
